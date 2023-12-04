e0953ca33212cddf7b13744686fdb048de580587
Saurav Pant <pantsaurav005@gmail.com>
2023-12-04 10:19:34 +0530

    Prisma with NextJS learning Done through this

--- Server/Server.js
+++ Server/Server.js
@@ -1,14 +1,81 @@
 const express = require("express");
-const app = express();
-const port = 5000;
+const { PrismaClient } = require("@prisma/client");
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "All Good",
-    status: 200,
+const prisma = new PrismaClient();
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+// GET all users with their posts
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
   });
+  res.json(users);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+// GET a user by ID with their posts
+app.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(id) },
+    include: {
+      posts: true,
+    },
+  });
+  res.json(user);
+});
+
+// POST a new user
+app.post("/users", async (req, res) => {
+  const { name, email } = req.body;
+  const newUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+  res.json(newUser);
+});
+
+// GET all posts with their author details
+app.get("/posts", async (req, res) => {
+  const posts = await prisma.post.findMany({
+    include: {
+      author: true,
+    },
+  });
+  res.json(posts);
+});
+
+// GET a post by ID with its author details
+app.get("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.post.findUnique({
+    where: { id: parseInt(id) },
+    include: {
+      author: true,
+    },
+  });
+  res.json(post);
+});
+
+// POST a new post
+app.post("/posts", async (req, res) => {
+  const { authorId } = req.body;
+  const newPost = await prisma.post.create({
+    data: {
+      authorId,
+    },
+  });
+  res.json(newPost);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 });
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,39 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/users");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        setUserData(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>Hii How are you man</h1>
+      <h2>User Data:</h2>
+      <ul>
+        {userData.map((user: any) => (
+          <li key={user.id}>
+            {user.name} - {user.email} - {user.posts[0].authorId}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
