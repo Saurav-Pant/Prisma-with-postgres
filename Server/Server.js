a0fd04bae9999fd5849787633d624e71b2a223e1
Saurav Pant <pantsaurav005@gmail.com>
2023-12-13 12:18:20 +0530

    1-1 done

--- Server/Server.js
+++ Server/Server.js
@@ -1,81 +1,52 @@
+// index.js
 const express = require("express");
+const bodyParser = require("body-parser");
 const { PrismaClient } = require("@prisma/client");
-const cors = require("cors");
 
 const prisma = new PrismaClient();
 const app = express();
-const PORT = 5000;
 
-app.use(express.json());
-app.use(cors());
+app.use(bodyParser.json());
 
-// GET all users with their posts
-app.get("/users", async (req, res) => {
-  const users = await prisma.user.findMany({
-    include: {
-      posts: true,
-    },
-  });
-  res.json(users);
-});
-
-// GET a user by ID with their posts
-app.get("/users/:id", async (req, res) => {
-  const { id } = req.params;
-  const user = await prisma.user.findUnique({
-    where: { id: parseInt(id) },
-    include: {
-      posts: true,
-    },
-  });
-  res.json(user);
-});
-
-// POST a new user
 app.post("/users", async (req, res) => {
-  const { name, email } = req.body;
-  const newUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-    },
-  });
-  res.json(newUser);
+  try {
+    const { email, profile } = req.body;
+    const user = await prisma.user.create({
+      data: {
+        email,
+        profile: {
+          create: {
+            name: profile.name,
+            gender: profile.gender,
+            age: profile.age,
+          },
+        },
+      },
+    });
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error - " + error.message);
+  }
 });
 
-// GET all posts with their author details
-app.get("/posts", async (req, res) => {
-  const posts = await prisma.post.findMany({
-    include: {
-      author: true,
-    },
-  });
-  res.json(posts);
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany({
+      include: {
+        profile: true,
+      },
+    });
+
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-// GET a post by ID with its author details
-app.get("/posts/:id", async (req, res) => {
-  const { id } = req.params;
-  const post = await prisma.post.findUnique({
-    where: { id: parseInt(id) },
-    include: {
-      author: true,
-    },
-  });
-  res.json(post);
-});
-
-// POST a new post
-app.post("/posts", async (req, res) => {
-  const { authorId } = req.body;
-  const newPost = await prisma.post.create({
-    data: {
-      authorId,
-    },
-  });
-  res.json(newPost);
-});
-
+const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
